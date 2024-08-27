b888cd0348a9112b6a7803f5c626e90f499dd2ca
bhubanesh-dev <bhubanesh.dev@gmail.com>
2024-08-27 13:16:23 +0530

    config server

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
   res.send(`Hello from Coder`);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ;
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
